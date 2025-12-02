c87eb8d32ce38a31bd54eb70b6a0c737152af289
Nantha001 <pulsernantha123@gmail.com>
2025-12-02 23:11:14 +0530

    hjhhj

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-const port = process.env.port;
+const port = process.env.port||3000;
 
 function auth(req, res, next) {
   const authHead = req.headers.authorization;
