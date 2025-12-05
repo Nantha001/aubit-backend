6be9155d7d477ae7a35ebb01af81c245bdfaf1cf
Nantha001 <pulsernantha123@gmail.com>
2025-12-06 01:19:14 +0530

    change

--- db/database.js
+++ db/database.js
@@ -1,27 +1,20 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
+const { Pool } = require("pg");
+const db = new Pool({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+});
 
-
-
-
-  const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-  });
-
-  db.connect((err) => {
-    if (err) {
-      
-      console.log("db Error",err)
-    }else{
-      console.log("db connected")
-    }
-  });
-
-
- 
-
-
+db.connect((err, client, release) => {
+  if (err) {
+    console.log("db Error", err);
+  } else {
+    console.log("db connected");
+    release();
+  }
+});
 
 module.exports = db;
