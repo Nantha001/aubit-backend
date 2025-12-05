5984d89f56755ab25f6e8f23dae885223d4e0b22
Nantha001 <pulsernantha123@gmail.com>
2025-12-06 00:49:58 +0530

    change

--- db/database.js
+++ db/database.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
-let db;
 
 
-  db = mysql.createConnection({
+
+  const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
