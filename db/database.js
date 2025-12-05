e0b6e04a9b37d299b31684711bf66e62c0aed271
Nantha001 <pulsernantha123@gmail.com>
2025-12-05 17:18:43 +0530

    change

--- db/database.js
+++ db/database.js
@@ -7,8 +7,7 @@ function connectDB() {
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-
+    database: process.env.DATABASE
   });
 
   db.connect((err) => {
