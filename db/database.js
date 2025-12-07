78f431afc33dd11da2625121b4bb3e96128ecf80
Nantha001 <pulsernantha123@gmail.com>
2025-12-07 17:51:38 +0530

    change

--- db/database.js
+++ db/database.js
@@ -1,21 +1,35 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.DB_PORT, 
-  family: 4                   
-});
+let db;
 
-db.connect((err) => {
-  if (err) {
-    console.log("Database Connection Failed ❌", err);
-    setTimeout(() => db.connect(), 2000); // retry
-  } else {
-    console.log("Database Connected ✔");
-  }
-});
+function connectDB() {
+  db = mysql.createConnection({
+    host: process.env.HOST,       
+    user: process.env.USER,      
+    password: process.env.PASSWORD, 
+    database: process.env.DATABASE, 
 
-module.exports = db;
+  });
+
+  db.connect((err) => {
+    if (err) {
+      console.log("Database Connection Failed ❌", err.code);
+      setTimeout(connectDB, 5000); 
+    } else {
+      console.log("Database Connected ✔");
+    }
+  });
+
+  db.on("error", (err) => {
+    console.log("DB Error", err.code);
+    if (err.code === "PROTOCOL_CONNECTION_LOST" || err.code === "ETIMEDOUT") {
+      connectDB();
+    } else {
+      throw err;
+    }
+  });
+}
+
+connectDB();
+
+module.exports = () => db;
