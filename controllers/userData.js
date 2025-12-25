97ffc4d23ac23d9f72cac6c199c739bfe179f539
Nantha001 <pulsernantha123@gmail.com>
2025-12-25 22:57:03 +0530

    data fix

--- controllers/userData.js
+++ controllers/userData.js
@@ -1,7 +1,7 @@
 const db = require("../db/database");
 
 function userData(req, res) {
-  const userQuery = `select name,reg_number,dob,gender from students`;
+  const userQuery = `select name,reg_number,dob,gender,profile_photo from students`;
   db.query(userQuery, [], (err, row) => {
     if (err) {
       return res.status(500).json({ message: "❌ Database failure" });
